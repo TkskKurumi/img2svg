3fad05ba405bfe149b42546347e0c105dacb2812
TkskKurumi <zafkielkurumi@gmail.com>
2021-08-19 16:26:59 +0800

    tweaks

--- simple_arg_parser.py
+++ simple_arg_parser.py
@@ -41,4 +41,6 @@ def parse_args(s,flag_start='-',default_flag='default'):
 		ret[flag]=True
 	return ret
 if(__name__=='__main__'):
-	print(parse_args(r'  千千 -suffix \"哒！\\ -u 402254524 -no_lines -nd'))
+	dic=parse_args(r'  千千 -suffix \"哒！\\ -u 402254524 enmiao az -no_lines -nd')
+	print(dic)
+	print(dic.get("dne"))
